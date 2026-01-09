11941e3a4cdb4eff17dbf76a560d8650a5a9aafd
River <myfm5u@gmail.com>
2026-01-09 17:47:29 +0900

    commit

---
+++ 2026-01-09/1-5-4/main.js
@@ -0,0 +1,6 @@
+import { add, sub } from "./math.js";
+import userProfile from "./userProfile.js";
+
+console.log(add(5, 3));
+console.log(sub(5, 3));
+console.log(userProfile);
---
+++ 2026-01-09/1-5-4/userProfile.js
@@ -0,0 +1,6 @@
+const userProfile = {
+  name: "Lime",
+  age: 20,
+};
+
+export default userProfile;
