7ad0f0279b45788cb6270b1508c3db2e95417ac8
River <myfm5u@gmail.com>
2026-02-03 16:32:09 +0900

    commit

---
+++ 2026-02-03/1-2-2/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Card />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ 2026-02-03/1-2-2/src/components/Card.jsx
@@ -0,0 +1,12 @@
+const Card = () => {
+  return (
+    <main>
+      <div className="card">
+        <h2>카드 제목</h2>
+        <p>카드 내용입니다.</p>
+      </div>
+    </main>
+  );
+};
+
+export default Card;
